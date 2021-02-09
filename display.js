0088ca63575bfb4063671c84d2155533a41f6e04
GabeHillZ <72522967+GabeHillZ@users.noreply.github.com>
2021-02-08 21:15:12 -0700

    Added chessboardjs, implemented as display engine, implemented Fischer's

--- display.js
+++ display.js
@@ -1,25 +1,58 @@
-var board = null
+const { Chess } = require('./chess.js')
+// const { Chessboard } = require('./chessboard')
+// const { Chessboard } = require('@chrisoakman/chessboardjs/dist/chessboard-1.0.0')
+// import * as Chessboard from "@chrisoakman/chessboardjs"
+
+let board = null
 
 let pieceString = () => {
-  //TODO implement
+  // the piece array
+  let pieceOrder = [],
+    // randomizer func
+    d = function (num) { return Math.floor(Math.random() * ++num) },
+    // func to find empty squares
+    emptySquares = function () {
+      let arr = []
+      for (let i = 0; i < 8; i++) if (pieceOrder[i] == undefined) arr.push(i)
+      return arr
+    }
+  // bishop on black
+  pieceOrder[d(2) * 2] = "b"
+  // bishop on white
+  pieceOrder[d(2) * 2 + 1] = "b"
+  // queen on empty
+  pieceOrder[emptySquares()[d(5)]] = "q"
+  // knight on empty
+  pieceOrder[emptySquares()[d(4)]] = "n"
+  // other knight
+  pieceOrder[emptySquares()[d(3)]] = "n"
+  // rooks and king between them
+  for (let x = 1; x <= 3; x++) pieceOrder[emptySquares()[0]] = x == 2 ? "k" : "r"
+  // compile the pieces into a string
+  let pieces = pieceOrder.join('')
+  //add pieces to string format for the chess library, uppercase the second set for the black pieces
+  return pieces + "/pppppppp/8/8/8/8/PPPPPPPP/" + pieces.toUpperCase() + " w KQkq - 0 1"
 }
 
-var game = new Chess()
+let piecess = pieceString()
+//init chess game with generated Fischer layout
+let game = new Chess(piecess)
 
+//randomize and automate the chess board (code taken from the chessboard library example)
 function makeRandomMove() {
-  var possibleMoves = game.moves()
+  let possibleMoves = game.moves()
 
   // exit if the game is over
   if (game.game_over()) return
 
-  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+  let randomIdx = Math.floor(Math.random() * possibleMoves.length)
   game.move(possibleMoves[randomIdx])
   board.position(game.fen())
 
   window.setTimeout(makeRandomMove, 500)
 }
-
-board = Chessboard('myBoard', 'start')
+//init the board
+board = Chessboard('myBoard', piecess)
+// board = Chessboard('myBoard')
 
 window.setTimeout(makeRandomMove, 500)
-
